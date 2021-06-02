fac427ce3e2254c894b69a513ae6a5721cc35a32
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-02 19:40:14 -0400

    complete linear and binary search

--- arrays/array_main.cpp
+++ arrays/array_main.cpp
@@ -15,25 +15,28 @@ struct Array
 
 };
 
+// prints contents of array
 void print_arr(Array arr)
 {
-    for (size_t i = 0; i < arr.size; i++)
+    for (size_t i = 0; i < arr.length; i++)
     {
         cout << arr.A[i] << " ";
     }
     cout << endl;
 };
 
+// pushes element end of array
 void push(Array *arr, int val)
 {
     arr->A[arr->length] = val; 
     arr->length++;
 }
 
+// inserts element at given index of array 
 void insert(Array *arr, int index, int val)
 {
 
-    if (arr->length < arr->size) 
+    if (index >= 0 && index <= arr->length && arr->length < arr->size) 
     {
         for (size_t i = arr->length; i >= index; i--)
         {
@@ -48,32 +51,76 @@ void insert(Array *arr, int index, int val)
             }
         }
     }
-    else
+    
+};
+
+// deletes element at given index of array, returns deleted element
+int remove_element(Array *arr,int index)
+{
+    int element = arr->A[index];
+    if(index <= arr->length && index >= 0)
     {
+        for (size_t i = index; i < arr->length - 1; i++)
+        {
+            arr->A[i] = arr->A[i+1]; 
+
+        }
+
+        arr->length--;
+    }
+    return element;
+    
+};
+
+// returns index of key if found, or returns -1 if element not found
+int linear_search(Array arr, int key)
+{
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] == key)
+        {
+            return i;
+        }
         
     }
 
+    return -1;
+};
 
+// returns index of key in sorted array or -1 if not found
+int binary_search(Array arr, int key, int first = -1, int last = -1)
+{   
+    if(first == -1)
+    { first = 0; }
+
+    if(last == -1)
+    { last = arr.length - 1; }
+
+    int middle = (last + first) / 2;
+
+    if(arr.A[middle] == key )
+    { return middle; }
+;
+    if (arr.A[middle]!= key && first == last)
+    { return -1; }
     
+    if(key < arr.A[middle])
+    { return binary_search(arr, key, first, last = middle - 1); }
 
+    else if (key > arr.A[middle])
+    { return binary_search(arr, key, first = middle + 1 ,last); }
     
-}
-
-
+};
 
 int main()
 {
-    Array arr = {{1,2,3}, 10, 3};
-    push(&arr, 10);
-    insert(&arr, 1, 69);
-    print_arr(arr);
     
+    Array arr = {{2,5,8,11,16,20,31,70}, 10, 8};
 
+    // print_arr(arr);
 
+    cout << linear_search(arr, 20) << endl;
+    cout << binary_search(arr, 20) << endl;
 
-
-
-    
-    
 
 }
