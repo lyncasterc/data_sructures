a5df761328d630ddacf761ea186a1355b3beb4bc
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-06 15:24:44 -0400

    complete Array class

--- arrays/Array.h
+++ arrays/Array.h
@@ -8,17 +8,235 @@
 #include <iomanip>
 
 using namespace std;
+template<class T>
 
 class Array
 {
     private:
-        int A[10]; // will point to array
+        T *A; // will point to array
         int size; // size allocatedd for arr
         int length; // number of elements in arr
 
-    public:
-    
+        //returns index of key if found, or -1 if not found
+        int linear_search(T key)
+        {
+            for (size_t i = 0; i < length; i++)
+            {
+                if (A[i] == key)
+                {
+                    return i;
+                }
+                
+            }
+            return -1;
+        };
 
+    public:
+        Array()
+        {
+            size = 20;
+            A = new T[20];
+            length = 0;
+        };
+
+        Array(T arr[], int len, int s)
+        {
+            size = s;
+            length = len;
+            A = new T[s];
+
+            for (size_t i = 0; i < len; i++)
+            {
+                A[i] = arr[i];
+            }
+
+        };
+
+        //prints contents of A
+        void print()
+        {
+            for (size_t i = 0; i < length; i++)
+            {
+                cout << A[i] << " ";
+            }
+            
+            cout << endl;
+        }
+
+        // sets the contents of A, changes length and size
+        void set_array(T arr[], int len, int s = 20)
+        {
+
+            length = len;            
+
+            if (s != 20)
+            {
+                size = s;
+                resize_array(s);
+            }
+            
+            for (size_t i = 0; i < len; i++)
+            {
+                A[i] = arr[i];
+            }
+
+        };
+        
+        //resizes A
+        void resize_array(int s)
+        {
+            T *arr = new int[s];
+
+            for (size_t i = 0; i < length; i++)
+            {
+                arr[i] = A[i];
+            }
+
+            delete []A;
+
+            A = arr;
+            arr = NULL;
+            
+        }
+        
+        //returns size
+        int get_size()
+        { return size; }
+
+        //returns length
+        int get_length()
+        { return length; }
+
+        //appends new element to end of A
+        void push(T val)
+        {
+            A[length] = val;
+            length++;
+        };
+
+        //insets new element at specific index of A
+        void insert(int index, T val)
+        {
+            if (index >= 0 && index <= length && length < size)
+            {
+                for (size_t i = length; i >= index; i--)
+                {
+                    if (i == index)
+                    {
+                        A[index] = val;
+                        length++;
+                    }
+                    else
+                    {
+                        A[i] = A[i-1];
+                    }
+                    
+                }
+                
+            }
+            
+        };
+
+        //removes element at specific index of A and returns it
+        T remove(int index)
+        {
+            T val = A[index];
+
+            if (index <= length && index >= 0)
+            {
+                for (size_t i = index; i < length - 1; i++)
+                {
+                    A[i] = A[i + 1];
+                }
+                
+                length--;
+            }
+        
+            return val;
+        };
+
+        //removes last element of A and returns it
+        T pop()
+        {
+            return remove(length - 1);
+        };
+        
+        //returns true if A is sorted, otherwise returns false
+        bool is_sorted()
+        {
+            for (size_t i = 0; i < length - 1; i++)
+            {
+                if (A[i] > A[i + 1])
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+            
+        }
+
+        //returns index of key if found, or -1 if not found
+        int find(T key)
+        { return linear_search(key); };
+
+        //returns element at A[index] or -1 if index is not valid
+        T get(int index)
+        {
+            if (index >= 0 && index < length)
+            {
+                return A[index];
+            }
+            return -1;
+        }
+
+        //replaces element at A[index] with val.
+        void replace(int index, T val)
+        {
+            if(index >=0 && index < length)
+            {
+                A[index] = val;
+            }
+        };
+
+        //reverse elements in A
+        void reverse()
+        {
+            int temp;
+            for (size_t i = 0; i < length / 2; i++)
+            {
+                temp = A[length - i - 1];
+                A[length - i - 1] = A[i];
+                A[i] = temp;
+            }
+            
+        }
+
+        //removes first element in A
+        void shift()
+        { remove(0); };
+
+        //rotates elements in A by the rotation factor, n
+        //Positive values for n rotate to the right and vice-versa.
+        void rotate(int n)
+        {
+            int arr2[length];
+
+            for (size_t i = 0; i < length; i++)
+            {
+                arr2[(i + n) % length] = A[i];
+            }
+            
+            set_array(arr2, length, size);
+        };
+
+        
+
+
+        ~Array()
+        {
+            delete []A;
+        };
 
 
 };
---
+++ arrays/struct_array.cpp
@@ -0,0 +1,364 @@
+#include <string>
+#include <array>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+struct Array
+{
+    int A[30]; // points to array in heap
+    int size; //size of array
+    int length; // # of elements in array
+
+};
+
+// prints contents of array
+void print_arr(Array arr)
+{
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        cout << arr.A[i] << " ";
+    }
+    cout << endl;
+};
+
+// pushes element end of array
+void push(Array *arr, int val)
+{
+    arr->A[arr->length] = val; 
+    arr->length++;
+};
+
+// inserts element at given index of array 
+void insert(Array *arr, int index, int val)
+{
+
+    if (index >= 0 && index <= arr->length && arr->length < arr->size) 
+    {
+        for (size_t i = arr->length; i >= index; i--)
+        {
+            if (i == index)
+            {
+                arr->A[index] = val;
+                arr->length++;
+            }
+            else
+            {
+                arr->A[i] = arr->A[i-1];
+            }
+        }
+    }
+    
+};
+
+// deletes element at given index of array, returns deleted element
+int remove_element(Array *arr,int index)
+{
+    int element = arr->A[index];
+    if(index <= arr->length && index >= 0)
+    {
+        for (size_t i = index; i < arr->length - 1; i++)
+        {
+            arr->A[i] = arr->A[i+1]; 
+
+        }
+
+        arr->length--;
+    }
+    return element;
+    
+};
+
+// returns index of key if found, or returns -1 if element not found
+int linear_search(Array arr, int key)
+{
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] == key)
+        {
+            return i;
+        }
+        
+    }
+
+    return -1;
+};
+
+// returns index of key in sorted array or -1 if not found
+int binary_search(Array arr, int key, int first, int last)
+{   
+    int middle;
+
+    if(arr.A[middle] == key )
+    { return middle; }
+;
+    if(last <= first)
+    {
+        middle = (last + first) / 2;
+
+        if(arr.A[middle] == key )
+        { return middle; }
+
+        else if(key < arr.A[middle])
+        { return binary_search(arr, key, first, last = middle - 1); }
+
+        else
+        { return binary_search(arr, key, first = middle + 1 ,last); } 
+
+    }
+    return -1;    
+    
+};
+
+int get(Array arr, int index)
+{
+    if(index >=0 && index < arr.length)
+    {
+        return arr.A[index];
+    }
+    return -1;
+
+};
+
+void replace(Array *arr, int index, int val)
+{
+    if(index >=0 && index < arr->length)
+    {
+        arr->A[index] = val;
+    }
+};
+
+int max(Array arr)
+{
+    int max = 0;
+
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] > max)
+        {
+            max = arr.A[i];
+        }
+    }
+    
+    return max;
+};
+
+int min(Array arr)
+{
+    int min = arr.A[0];
+
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] < min)
+        {
+            min = arr.A[i];
+        }
+    }
+    
+    return min;
+};
+
+int sum(Array arr)
+{
+    int sum = 0;
+
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        
+        sum += arr.A[i];
+        
+    }
+    
+    return sum;
+}
+
+double average(Array arr)
+{
+    return sum(arr) / static_cast <double> (arr.length);
+}
+
+// reverses array in place
+void reverse(Array *arr)
+{
+    int temp;
+
+    for (size_t i = 0; i < arr->length / 2; ++i)
+    {
+        temp = arr->A[arr->length - i - 1];
+        arr->A[arr->length - i - 1] = arr->A[i];
+        arr->A[i] = temp;
+    }
+
+};
+
+// removes first elements
+void shift(Array &arr)
+{
+    remove_element(&arr, 0);
+};
+
+void rotate_left(Array *arr)
+{
+    int arr2[arr->length];
+
+    for (size_t i = 0; i < arr->length; i++)
+    {
+        arr2[(i - 1) % arr->length] = arr->A[i];
+    }
+    
+    for (size_t i = 0; i < arr->length; i++)
+    {
+        arr->A[i] = arr2[i];
+    }
+
+};
+
+void rotate_right(Array *arr)
+{
+    int arr2[arr->length];
+
+    for (size_t i = 0; i < arr->length; i++)
+    {
+        arr2[(i + 1) % arr->length] = arr->A[i];
+    }
+    
+    for (size_t i = 0; i < arr->length; i++)
+    {
+        arr->A[i] = arr2[i];
+    }
+}
+
+bool is_sorted(Array arr)
+{
+    for (size_t i = 0; i < arr.length - 1; i++)
+    {
+        if(arr.A[i] > arr.A[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+};
+
+Array* merge(Array arr1, Array arr2)
+{   
+    int i = 0, j = 0 , k = 0;
+    Array *arr3 = new Array;
+
+    while (i < arr1.length && j < arr2.length)
+    {
+        if (arr1.A[i] < arr2.A[j])
+        {
+            arr3->A[k++] = arr1.A[i++];
+        }
+        else
+        {
+            arr3->A[k++] = arr2.A[j++];
+            
+        }
+        
+    }
+
+    while (i < arr1.length)
+    {
+        arr3->A[k++] = arr1.A[i++];
+    }
+
+    while (j < arr2.length)
+    {
+        arr3->A[k++] = arr2.A[j++];
+    }
+    
+    arr3->length = arr1.length + arr2.length;
+    arr3->size = 30;
+    
+    return arr3;
+};
+
+Array* set_union(Array arr1, Array arr2)
+{
+    Array *arr3 = new Array;
+
+    int i = 0, k = arr1.length;
+
+    for (size_t i = 0; i < arr1.length; i++)
+    {
+        arr3->A[i] = arr1.A[i];
+    }
+
+    while (i < arr2.length)
+    {
+        if (linear_search(arr1, arr2.A[i]) == -1)
+        {
+            arr3->A[k++] = arr2.A[i];
+        }
+        i++;
+    }
+    
+    arr3->length = k;
+    arr3->size = 30;
+
+    return arr3;
+
+};
+
+Array * set_difference(Array arr1, Array arr2)
+{
+    Array *arr3 = new Array;
+    int k = 0, result;
+    
+    for (size_t i = 0; i < arr1.length; i++)
+    {
+        result = linear_search(arr2, arr1.A[i]);
+
+        if ( result == -1 )
+        {
+            arr3->A[k++] = arr1.A[i];
+        }
+        
+    }
+    
+    arr3->length = k;
+    arr3->size = 30;
+
+    return arr3;
+
+};
+
+Array * set_intersection(Array arr1, Array arr2)
+{
+    Array *arr3 = new Array;
+
+    int k = 0;
+
+    for (size_t i = 0; i < arr1.length; i++)
+    {
+        if ( linear_search(arr2, arr1.A[i]) != -1 )
+        {
+            arr3->A[k++] = arr1.A[i];
+        }
+    }
+    
+    arr3->length = k;
+    arr3->size = 30;
+
+    return arr3;
+};
+
+
+int main()
+{
+    
+    Array arr1 = {{2,6,10,15,25}, 30, 5};
+    Array arr2 = {{3,6,7,15,20}, 30, 5};
+    Array *arr3;
+
+    cout << binary_search(arr1, 1, 0, arr1.length) << endl;
+    // print_arr(*arr3);
+
+    
+
+
+}
