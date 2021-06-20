f54d0e9ba9f4a5412e92cc81811320a604955feb
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-19 23:25:12 -0400

    finish solution_2 in parenthesis_match.cpp

--- stack/paranthesis_match.cpp
+++ stack/paranthesis_match.cpp
@@ -34,8 +34,65 @@ bool solution(string str)
     { return false; }
 
     return true;
-
 };
+
+//checks ({[]})
+bool solution_2(string str)
+{
+    ListStack stack;
+
+    for (size_t i = 0; str[i]!= '\0'; i++)
+    {
+        if(str[i] == '(' || str[i] == '{' || str[i] == '[')
+        { stack.push(str[i]); }
+
+        else if (str[i] == ')')
+        {
+            if(stack.is_empty())
+            { return false; }
+            else
+            {
+                if(stack.peek(1) == '(' )
+                { stack.pop(); }
+                else
+                { return false; }
+            }
+            
+        }
+        else if (str[i] == '}')
+        {
+            if(stack.is_empty())
+            { return false; }
+            else
+            {
+                if(stack.peek(1) == '{')
+                { stack.pop(); }
+                else
+                { return false; }
+            }
+            
+        }
+        else if (str[i] == ']')
+        {
+            if(stack.is_empty())
+            { return false; }
+            else
+            {
+                if(stack.peek(1) == '[')
+                { stack.pop(); }
+                else
+                { return false; }
+            }
+            
+        }  
+    }
+
+    if (!stack.is_empty())
+    { return false; }
+
+    return true;
+    
+}
 
 int main()
 {
@@ -43,10 +100,14 @@ int main()
     string str_2 = "(((a+b)*(c-d))";
     string str_3 = "((a+b)*(c-d)))";
 
+    string str_4 = "{([a+b]*[c-d])}}";
+
     cout << solution(str_3) << endl;
+    cout << solution_2(str_4) << endl;
 
     
 
     
 
+    
 }
