1b4331037ca3b75469cc70c2502f9ffc01be5309
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-02 16:37:14 -0400

    write ll_rotation() in avl_tree.cpp

--- trees/BinaryTree/BinarySearchTree/avl_tree.cpp
+++ trees/BinaryTree/BinarySearchTree/avl_tree.cpp
@@ -5,8 +5,8 @@ using namespace std;
 class Node
 {
     public:
-        Node *left;
-        Node *right;
+        Node *left = NULL;
+        Node *right = NULL;
         int height;
         int key;
 };
@@ -16,13 +16,53 @@ class AVLTree
     public:
         Node *root = NULL;
 
+
+        int height(Node *node)
+        {
+            if(!node)
+            { return 0; }
+            return node->height;
+            
+        }
+
+        int max(int a, int b)
+        { return (a > b) ? a : b; }
+
+        int get_balance_factor(Node *node)
+        {
+            if(!node)
+            { return 0; }
+            return height(node->left) - height(node->right);
+        }
+
+        Node * ll_rotation(Node *node)
+        {
+            Node *nl = node->left;
+            Node *nlr = nl->right;
+
+            //rotation
+            nl->right = node;
+            node->left = nlr;
+
+            // updating heights of nodes
+            node->height = 1 + max(height(node->left), height(node->right));
+            nl->height = 1 + max(height(nl->left), height(nl->right));
+            
+            //updating root if rotation was performed around root node
+            if(root == node)
+            { root = nl; }
+
+            return nl;
+
+        }
+
         Node *insert(Node *node, int key)
         {
             if(!node)
             {
                 node = new Node;
                 node->key = key;
-                node->height = 0;
+                node->height = 1;
                 return node;
             }
 
@@ -31,20 +71,26 @@ class AVLTree
 
             else if(key > node->key)
             { node->right = insert(node->right, key); }
+                
+            node->height = 1 + max(height(node->left), height(node->right));
+
+            if(get_balance_factor(node) == 2 && get_balance_factor(node->left) == 1)
+            { return ll_rotation(node); }
+
+            return node; 
 
 
-            return node;
-        }
+        }        
 
-        int height(Node *node)
-        {
-            if(!node)
-            { return 0; }
-            return node->height;
-        }
+        
 };
 
 int main()
 {
+    AVLTree t;
+    t.root = t.insert(t.root, 10);
+    t.root = t.insert(t.root, 5);
+    t.root = t.insert(t.root, 1);
 
+    cout << t.root->key;
 }
