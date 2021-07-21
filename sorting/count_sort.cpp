d5b785e9b6bc0bb41205e432aeb541fee010ece1
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-21 18:30:22 -0400

    refactor count_sort

--- sorting/count_sort.cpp
+++ sorting/count_sort.cpp
@@ -4,9 +4,11 @@
 using std::cout;
 using std::endl;
 
+// takes a lot of space 
 void count_sort(int arr[], int size)
 {
-    int max = 0, j = 0;
+    int max = 0, j = 0, *hash_arr;
+    //finding max 
     for (auto i = 0; i < size; i++)
     {
         if(arr[i] > max)
@@ -14,19 +16,21 @@ void count_sort(int arr[], int size)
     }
 
     max++;
-    
-    int hash_arr[max]; 
+    hash_arr = new int[max];
 
+    // initializing hash_arr w/ 0
     for (auto i = 0; i < max; i++)
     {
         hash_arr[i] = 0;
     }
 
+    // hashing arr in hash_arr
     for (auto i = 0; i < size; i++)
     {
         hash_arr[arr[i]] += 1; 
     }
 
+    // copying hash values for each index back into arr
     for (auto i = 0; i < max; i++)
     {
         while (hash_arr[i] > 0)
@@ -35,8 +39,7 @@ void count_sort(int arr[], int size)
             hash_arr[i]--;
         }
     }
-    
-    
+
 }
 
 
@@ -47,7 +50,6 @@ int main()
     int nums[] = {6, 3, 9, 10, 15, 6, 8, 12, 3, 6};
     int size = sizeof(nums) / sizeof(nums[0]);
     count_sort(nums, size);
-
     for (auto &&i : nums)
     {
         cout << i << " ";
